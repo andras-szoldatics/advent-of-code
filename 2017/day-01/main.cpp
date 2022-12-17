260e74aaa72b0075e1636d8df3454235b63ee3d7
András Szoldatics <github@szolda.me>
2022-12-17 16:17:58 +0100

    added project for 2017/day-01

---
+++ 2017/day-01/main.cpp
@@ -0,0 +1,57 @@
+// C++ standard includes
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // temporary storage
+    string line;
+
+    // read input file
+    ifstream input("input.txt");
+
+    // read first & only relevant line
+    getline(input, line);
+    const size_t halfAhead = (line.size() / 2);
+
+    // running captcha
+    int64_t firstCaptcha = 0;
+    int64_t secondCaptcha = 0;
+
+    // process line
+    for (size_t current = 0; current < line.size(); current++) {
+        // get next index
+        size_t next = (current + 1);
+        if (next >= line.size()) {
+            next -= line.size();
+        }
+
+        // check if the two characters match
+        if (line.at(current) == line.at(next)) {
+            // add to first captcha
+            int64_t number = (line.at(current) - '0');
+            firstCaptcha += number;
+        }
+
+        // get next index
+        next = (current + halfAhead);
+        if (next >= line.size()) {
+            next -= line.size();
+        }
+
+        // check if the two characters match
+        if (line.at(current) == line.at(next)) {
+            // add to second captcha
+            int64_t number = (line.at(current) - '0');
+            secondCaptcha += number;
+        }
+    }
+
+    cout << "first CAPTCHA : " << firstCaptcha << endl;
+    cout << "second CAPTCHA : " << secondCaptcha << endl;
+
+    return 0;
+}
